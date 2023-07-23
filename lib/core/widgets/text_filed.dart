8cf4964093d3819ee99df8e3335bc3ea094d7ff9
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-07-23 12:55:33 +0300

    add login button

---
+++ lib/core/widgets/text_filed.dart
@@ -0,0 +1,62 @@
+
+import 'package:flutter/material.dart';
+
+class CustomTextFiled extends StatelessWidget {
+  const CustomTextFiled({Key? key,
+    this.onChange ,
+    required this.hint,
+    this.maxLines=1,
+    this.onSaved,
+    this.keyboardType=TextInputType.text,
+  }) : super(key: key) ;
+
+  final String ?hint;
+  final void Function(String?)? onSaved;
+  final Function(String) ? onChange;
+  final int maxLines;
+  final TextInputType keyboardType;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onChanged: onChange,
+      onSaved: onSaved,
+      keyboardType:keyboardType ,
+      validator: (val){
+        if(val?.isEmpty??true){
+          return 'Field is required';
+        }
+        else {
+          return null;
+        }
+      },
+      maxLines: maxLines,
+      cursorColor: const Color(0xFF1FC9C2),
+      decoration: InputDecoration(
+         fillColor: Colors.grey.shade100,
+          filled: true,
+          label: Text(hint!,style:  TextStyle(
+            color: Colors.grey.shade600,
+            fontWeight: FontWeight.w600,
+          ),),
+       //   errorStyle: Styles.textStyle14.copyWith(fontSize: 12),
+          // labelStyle: Styles.textStyle24,
+
+          focusedBorder: buildBorder(border: 8,color: Colors.black ),
+          enabledBorder: buildBorder(border: 8,color: Colors.grey),
+          border:buildBorder(border: 8, color: Colors.grey,)
+      ),
+    );
+  }
+
+}
+OutlineInputBorder buildBorder({
+  required double border,
+  required Color color,
+}) {
+  return OutlineInputBorder(
+      borderSide:  BorderSide(
+       color: color,
+      ),
+      borderRadius: BorderRadius.circular(border)
+  );
+}
--- lib/features/login/presentation/views/widgets/login_view_body.dart
+++ lib/features/login/presentation/views/widgets/login_view_body.dart
@@ -1,15 +1,64 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:travel_booking/core/widgets/text_filed.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final size=MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size;
     return Column(
       children: [
         SignInContainer(size: size),
+        const SizedBox(
+          height: 32,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            children: [
+              const CustomTextFiled(hint: 'Email'),
+              const SizedBox(
+                height: 15,
+              ),
+              const CustomTextFiled(hint: 'Password'),
+              const SizedBox(
+                height: 6,
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                    value: true,
+                    onChanged: (val) {},
+                    activeColor: const Color(0xFFFB9057),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                  ),
+                  const Text('Remember Me'),
+                  Spacer(),
+                  const Text('Forget Password ?',),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Sign In',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: TextButton.styleFrom(
+                    minimumSize: Size(size.width, 66),
+                    backgroundColor: Color(0xFFFB9057),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16))),
+              )
+            ],
+          ),
+        )
       ],
     );
   }
@@ -26,26 +75,33 @@ class SignInContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: size.height/4,
+      height: size.height / 4,
       width: size.width,
       alignment: Alignment.bottomCenter,
       padding: const EdgeInsets.symmetric(vertical: 30),
       decoration: const BoxDecoration(
-        color: Color(0xFF1FC9C2),
-        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(32))
-      ),
+          color: Color(0xFF1FC9C2),
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(32))),
       child: const Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Text('Sign In',style: TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
-          ),),
-          SizedBox(height: 10,),
-          Text('Hi, Welcome back! ',style: TextStyle(
-            color: Colors.white,
-          ),),
+          Text(
+            'Sign In',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Hi, Welcome back! ',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
         ],
       ),
     );
