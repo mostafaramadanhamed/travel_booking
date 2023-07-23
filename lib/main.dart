475685d2a60a0f8d5aa111836aca4950606ed5cd
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-07-23 12:05:14 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'features/login/presentation/views/login.dart';
+
+void main() {
+  runApp(const TravelBookingApp());
+}
+
+class TravelBookingApp extends StatelessWidget {
+  const TravelBookingApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Travel Booking App',
+
+      home: LoginView(),
+    );
+  }
+}
