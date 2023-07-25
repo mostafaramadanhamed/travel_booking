a0b86c50c7462a368fc9311d1e4af3288371f293
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-07-25 10:41:04 +0300

    refactor color

---
+++ lib/core/app_color.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+abstract class AppColor{
+
+  static const kPrimaryColor=Color(0xFF1FC9C2);
+  static const kSecondary=Color(0xFFFB9057);
+  static const kBottomNavIconColor=Color(0xFFBFC0C2);
+
+}
